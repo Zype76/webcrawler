a489700811159a6592f7f4e52827e46ee1131cfe
Zype76 <zype76@gmail.com>
2016-08-16 15:28:37 -0400

    first commit with image download script

---
+++ downloadimage.py
@@ -0,0 +1,12 @@
+import random
+import urllib.request
+
+
+def download_web_image(url):
+    name = random.randrange(1, 1000)
+    full_name = str(name) + ".jpg"
+    urllib.request.urlretrieve(url, full_name)
+
+dl_link = input("Image Link:")
+
+download_web_image(dl_link)
