cb7c76c7b102ebb3faad86da4340c7b031b36a4e
Zype76 <zype76@gmail.com>
2016-08-16 15:53:29 -0400

    Added comments and python interpreter path

--- downloadimage.py
+++ downloadimage.py
@@ -1,12 +1,18 @@
+#!/usr/bin/python3.4
 import random
 import urllib.request
 
 
 def download_web_image(url):
+    #generate random number to prevent naming conflicts:
     name = random.randrange(1, 1000)
+    #append a .jpg onto the random name:
     full_name = str(name) + ".jpg"
+    #download image from requested url and give it the random name:
     urllib.request.urlretrieve(url, full_name)
 
+#prompt for a download link: 
 dl_link = input("Image Link:")
 
+#run the function created above to download the image from the provided link:
 download_web_image(dl_link)
